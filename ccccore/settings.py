77034abecd43e469d89caf50f6b848ecf1a1ade0
codingwithchad <codingwithchad@gmail.com>
2024-01-15 19:24:58 -0800

    updated allowed hosts

--- ccccore/settings.py
+++ ccccore/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("MY_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['django-server-production-d06f.up.railway.app', 'www.caloricreditcard.com']
+ALLOWED_HOSTS = ['django-server-production-d06f.up.railway.app', 'www.caloriecreditcard.com', 'caloriecreditcard.com', ]
 
 
 # Application definition
